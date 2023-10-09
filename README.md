083966e58a6969c6b53458a6d6bba59d95cf6515
ShinyShoe <mcshinyshoe@gmail.com>
2023-10-09 17:27:21 +0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quick Start
+```bash
+apt-get ca-certificates && curl https://github.com/McShinyShoe/JarkomGNS3/blob/master/run.sh && chmod +x ./run.sh && bash ./run.sh
+
+```
---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+print() {
+    echo -e "\033[0;33m"$@"\033[0m"
+}
+
+read -p "Choose which module do u want to start
+1) Modul GNS
+Modul: " OPT
+
+HOSTNAME=$(hostname)
+
+case $OPT in
+    1)
+        MODUL=ModulGNS
+        ;;
+esac
+
+print "Configuring" $MODUL "for node" $HOSTNAME
+
+curl -o configure.sh https://raw.githubusercontent.com/McShinyShoe/JarkomGNS3/master/$MODUL/$HOSTNAME.sh
+
+chmod +x ./configure.sh
+
+bash ./configure.sh
